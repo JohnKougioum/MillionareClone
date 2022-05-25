97f8352b12d8a4da5d6ba7ade82160984c263e32
John Kougioumtzidis <jkougioumtzidis@gmail.com>
2022-05-25 10:43:58 +0300

    Initialize

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,47 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="h-screen w-full overflow-hidden">
+    <img
+      class="w-full h-full z-0 absolute image__blur"
+      src="../assets/stage.jpg"
+      alt="HTML5"
+    />
+    <div
+      class="
+        w-full
+        md:w-2/3
+        h-full
+        relative
+        left-1/2
+        transform
+        -translate-x-1/2
+        z-10
+      "
+    >
+      <div class="h-full w-full flex flex-col justify-evenly items-center">
+        <div class="h-auto flex justify-center">
+          <img class="md:h-96" src="../assets/better_logo.png" alt="HTML5" />
+        </div>
+        <div class="text-white text-3xl">
+          <button class="relative py-4 px-48 play__button">PLAY</button>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+};
 </script>
+
+<style scoped>
+.image__blur {
+  filter: blur(4px);
+  -webkit-filter: blur(4px);
+}
+
+.play__button {
+  background-color: #040444;
+}
+</style>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+  theme: {
+    extend: {},
+    // colors: {
+    //   main_purple: "#040444",
+    // },
+  },
+  plugins: [],
+};
